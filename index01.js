788f9ef05c0fe63bc08e2704ef7003995e8e3798
unknown <853471229@qq.com>
2019-07-11 10:06:39 +0800

    基本类型

---
+++ index01.js
@@ -0,0 +1,9 @@
+var a = 123;
+var b = 'zzh';
+var c = [123, 123];
+var d = ["杨过", "令狐冲"];
+var username = "zzh";
+var e = [123, "123", true];
+var f = ["杨过", "令狐冲"];
+var g = [12, false];
+console.log(g);
---
+++ index01.ts
@@ -0,0 +1,9 @@
+var a: number = 123;
+var b: string = 'zzh';
+var c: number[] = [123, 123];
+var d: [string, string] = ["杨过", "令狐冲"];
+var username: string = "zzh";
+var e: any[] = [123, "123", true];
+var f: Array<string> = ["杨过", "令狐冲"];
+var g: Array<any> = [12, false];
+console.log(g)
