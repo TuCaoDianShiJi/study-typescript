a49083cb4241034fd35901e20dd5efa8b956b43b
unknown <853471229@qq.com>
2019-08-05 11:02:51 +0800

    函数重载

--- part2/index09.ts
+++ part2/index09.ts
@@ -1,9 +1,11 @@
 // 函数重载 允许用相同名字与不同参数来创建多个函数
+// 没有实现的定义
 function sum (x:number, y: number): number;
 function sum (x:number, y: number, z: number): number;
 
+// 上面两种定义的组合实现
 function sum(x: number, y: number, z?: number): number{
-    if(z === undefined){
+    if(typeof z === 'undefined'){
         return x + y;
     }else{
         return x + y + z;
