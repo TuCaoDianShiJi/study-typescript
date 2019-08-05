82284953d5d7b8cc33be62842f3d6d40fc6d4b2d
unknown <853471229@qq.com>
2019-08-05 10:34:01 +0800

    处理列表数据

---
+++ part2/index07.js
@@ -0,0 +1,19 @@
+var Todo2 = /** @class */ (function () {
+    function Todo2() {
+    }
+    return Todo2;
+}());
+var todos = [
+    {
+        id: 1,
+        name: '张无忌',
+        age: 18,
+        adress: '武当'
+    },
+    {
+        id: 2,
+        name: '杨过',
+        age: 18,
+        adress: '古墓派'
+    }
+];
---
+++ part2/index07.ts
@@ -0,0 +1,28 @@
+interface Todo1 {
+    id: number;
+    name: string;
+    age: number;
+    adress: string;
+}
+
+class Todo2{
+    id: number;
+    name: string;
+    age: number;
+    adress: string;
+}
+
+let todos: Todo2[] = [
+    {
+        id: 1,
+        name: '张无忌',
+        age: 18,
+        adress: '武当'
+    },
+    {
+        id: 2,
+        name: '杨过',
+        age: 18,
+        adress: '古墓派'
+    }
+]
